d031ab1dda501a09b9a81f263a863fde7c890031
Breyner Parada <breyner@quepar-solutions.com>
2025-03-19 10:20:21 -0500

    pagination and search functionality added

--- app/query/route.ts
+++ app/query/route.ts
@@ -1,23 +1,19 @@
 import postgres from 'postgres';
 
-const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
+const sql = postgres(process.env.POSTGRES_URL_DIRECT!, { ssl: "require" });
 
 async function listInvoices() {
 	const data = await sql`
     SELECT invoices.amount, customers.name
     FROM invoices
     JOIN customers ON invoices.customer_id = customers.id
-    WHERE invoices.amount = 666;
+    WHERE invoices.amount = 5877;
   `;
 
 	return data;
 }
 
 export async function GET() {
-  return Response.json({
-    message:
-      'Uncomment this file and remove this line. You can delete this file when you are finished.',
-  });
   try {
   	return Response.json(await listInvoices());
   } catch (error) {
